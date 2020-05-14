2e186a4636ffdfe7f4cfd98e9804d470d0946784
huadongf <huadongff@qq.com>
2020-05-14 14:08:50 +0800

    增加了验证码的干扰项

--- src/main/java/session/CodeServlet.java
+++ src/main/java/session/CodeServlet.java
@@ -16,28 +16,18 @@ import javax.servlet.http.HttpSession;
 public class CodeServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)throws IOException {
-        //验证码的servlet
-        //0.创建空白图片
         BufferedImage image = new BufferedImage(100,30,BufferedImage.TYPE_INT_RGB);
-        //1.获取图片画笔
         Graphics g = image.getGraphics();
         Random r = new Random();
-        //2.设置画笔颜色
         g.setColor(new Color(r.nextInt(255),r.nextInt(255),r.nextInt(255)));
-        //3.绘制矩形的背景
         g.fillRect(0, 0, 100, 30);
-        //4.调用自定义的方法,获取长度为5的字母数字组合的字符串
         String number = getNumber();
-        //获得session
         HttpSession session = request.getSession();
-        //将这5个随机字符绑定到session中
         session.setAttribute("code", number);
         g.setColor(new Color(0,0,0));
         g.setFont(new Font(null,Font.BOLD,24));
-        //5.设置颜色字体后,绘制字符串
         g.drawString(number, 5, 25);
-        //6.绘制8条干扰线
-        for(int i=0;i<8;i++){
+        for(int i=0;i<30;i++){
             g.setColor(new Color(r.nextInt(255),r.nextInt(255),r.nextInt(255),r.nextInt(255)));
             g.drawLine(r.nextInt(100), r.nextInt(30), r.nextInt(100), r.nextInt(30));
         }
